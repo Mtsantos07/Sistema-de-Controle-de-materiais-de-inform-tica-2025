c07b904a57ec989e42f152fff05e2a6983a952c2
Mtsantos07 <mtsantos015@gmail.com>
2025-11-17 12:32:19 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+index.php
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistema de Controle de Materiais de Informática</title>
+    <link rel="stylesheet" href="index.css">
+     <link rel="stylesheet" href="index.html">
+</head>
+
+<body>
+
+    <!-- Cabeçalho -->
+    <header>
+        <div class="container">
+            <h1>Sistema de Controle de Materiais de Informática</h1>
+            <nav>
+                <ul>
+                 
+  <div class="logo"></div>
+  <a href="equipamento.php">Equipamento</a>
+  <a href="painel_estoque.php">estoque</a>
+  <a href="associados.html">associados</a>
+  <a href="feedback.html">feedback</a>
+  <a href="login.php">login</a>
+  <a href="criar-conta.php">criar conta</a>
+</nav>
+            </nav>
+        </div>
+    </header>
+    <!-- Seção Sobre -->
+    <section id="sobre">
+        <div class="container">
+            <h2>Sobre o Sistema</h2>
+            <p>Este sistema foi desenvolvido para gerenciar e controlar o uso de materiais de informática, como computadores, projetores, periféricos e outros equipamentos. Ele permite a consulta de equipamentos disponíveis, empréstimos, reservas e manutenções realizadas.</p>
+        </div>
+    </section>
