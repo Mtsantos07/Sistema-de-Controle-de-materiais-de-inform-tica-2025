c37af1b2dc91226de149c3868d9cb3e79bd375c1
Mtsantos07 <mtsantos015@gmail.com>
2025-11-17 12:33:26 -0300

    Create painel_estoque.php

---
+++ painel_estoque.php
@@ -0,0 +1,62 @@
+painel_estoque.php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>Painel de Controle - Sistema de Controle de Materiais</title>
+<link rel="stylesheet" href="index.css">
+<nav>
+  <div class="logo"></div>
+  <a href="equipamento.php">equipamento</a>
+  <a href="estoque.php">estoque</a>
+  <a href="reservas.php">reservas</a>
+  <a href="feedback.html">feedback</a>
+  <a href="login.php">login</a>
+  <a href="criar-conta.php">criar conta</a>
+</nav>
+
+
+<main>
+  <!-- Estoque -->
+  <section id="estoque" class="container">
+    <div class="section-title">Estoque</div>
+    <div class="grid-items">
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Teclados</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Projetores</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Caixas de Som</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Mouse</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Computadores</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Cabos</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Headset</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Monitores</div>
+      </div>
+      <div class="card" tabindex="0">
+        <div class="card-icon"></div>
+        <div class="card-label">Gabinetes</div>
+      </div>
+    </div>
+  </section>
